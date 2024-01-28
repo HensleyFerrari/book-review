865b4e3875ebd5f1ea89002897d0a9dbd069bb44
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-01-28 18:04:41 -0300

    feat: added the logic for filter

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -21,7 +21,7 @@
         @endphp
 
         @foreach ($filters as $key => $label)
-            <a href="{{ route('books.index', [...request()->$query(), 'filter' => $key]) }}"
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
                 class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
                 {{ $label }}
             </a>
